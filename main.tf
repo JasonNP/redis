e75b897d0cf0984d87d5c57b8ccd9f9187c08866
JasonNP <48024163+JasonNP@users.noreply.github.com>
2019-07-11 12:25:33 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_redis_cache" "redis" {
+  name                = "${var.redis_name}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group_name}"
+  capacity            = "${var.capacity}"
+  family              = "${var.family}"
+  sku_name            = "${var.sku_name}"
+  enable_non_ssl_port = true
+  minimum_tls_version = "${var.minimum_tls_version}"
+
+  redis_configuration {}
+}
